87a0e0105a803c834c170c4376554a9f43237987
Bryan Deng <bryan.deng002@gmail.com>
2026-03-11 23:17:17 -0400

    infra: fix depends_on and minimize init.sql

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -1,17 +1,3 @@
-CREATE TABLE datasets (
-    id BIGSERIAL PRIMARY KEY,
-    created_at TIMESTAMP(6) NOT NULL DEFAULT now(),
-    dataset_name TEXT NOT NULL,
-    dataset_version TEXT NOT NULL,
-    timestamp TIMESTAMP(6) NOT NULL,
-    data JSONB NOT NULL
-);
-
--- non-unique index for range queries on
--- (dataset_name, dataset_version, timestamp)
-CREATE INDEX idx_datasets_name_version_ts
-ON datasets (dataset_name, dataset_version, timestamp);
-
 -- read-only role for the pgweb UI
 CREATE USER datastream_readonly WITH PASSWORD 'readonly';
 GRANT CONNECT ON DATABASE datastream TO datastream_readonly;
