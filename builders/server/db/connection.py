dce06c55eb09c79c833591544fcdbee6faa0e57e
Bryan Deng <bryan.deng002@gmail.com>
2026-03-15 23:25:19 -0400

    feat: add structlog logging to db layer

--- builders/server/db/connection.py
+++ builders/server/db/connection.py
@@ -1,6 +1,9 @@
 import os
 
 import psycopg2
+import structlog
+
+logger = structlog.get_logger()
 
 _pool = None
 
@@ -9,5 +12,6 @@ def get_conn():
     """Get a database connection (simple single-connection approach for MVP)."""
     global _pool
     if _pool is None or _pool.closed:
+        logger.debug("creating new database connection")
         _pool = psycopg2.connect(os.environ["DATABASE_URL"])
     return _pool
